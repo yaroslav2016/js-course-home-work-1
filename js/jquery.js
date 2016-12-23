d2b8710a2a15e304d2b778f0d0ecc2a4a414a33c
yaroslav2016 <yaroslavrokhmanov@gmail.com>
2016-12-24 00:32:08 +0300

    24.12.2016

---
+++ js/jquery.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+$('.baner').slideUp(100).slideDown(4000);
+//$('.decktop').fadeOut(3000).fadeIn(3000);
+$(".decktop").animate({left: '40px'},2000);
+$(".block1").animate({right: '40px'},2000);
+$('.bg-success').fadeOut(100).fadeIn(7000);
+
+});
+
+
+
+
+
+
+
+
+
